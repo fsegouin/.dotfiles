b5dd9c8133a086608b5af06724105cdcb6e9c319
Florent Segouin <florent@segouin.me>
2017-10-12 10:40:23 +0100

    Add aliases to fish

---
+++ fish/aliases.fish
@@ -0,0 +1,8 @@
+# Aliases
+# cd
+alias ... 'cd ../../'
+alias .... 'cd ../../../'
+alias ..... 'cd ../../../../'
+
+# Apps
+alias iossimulator 'open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,13 @@
+# Load aliases
+. ~/.config/fish/aliases.fish
+
+# Ruby stuff
 rvm default
+
+# Go stuff
 set -x GOPATH $HOME/.go
 set -x GOROOT /usr/local/opt/go/libexec
-set PATH $GOPATH/bin $GOROOT/bin $PATH
+set PATH $GOROOT/bin $PATH
+
+# nvm stuff
+set PATH $HOME/.nvm/versions/node/(node -v)/bin $PATH
